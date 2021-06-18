1de2645e7e04defde5780fb803bc4e82be93081b
Rainforest <rainforestnick@gmail.com>
2021-06-18 22:58:19 +0800

    modify name card

--- lib/widgets/clip_bdrs.dart
+++ lib/widgets/clip_bdrs.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
 import 'package:personal_website/widgets/fitted_child_builder.dart';
 
 class ClipBDRS extends StatefulWidget {
--- lib/widgets/name_card.dart
+++ lib/widgets/name_card.dart
@@ -8,6 +8,7 @@ class NameCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
       child: Padding(
         padding: const EdgeInsets.all(30),
         child: Column(
